b97a248c2fffa98f33a4b057d4da9b795b39aa01
Goi Jia Jian <jiajiannn@gmail.com>
2021-04-03 17:35:41 +0800

    feat: update

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
-const express = require('express');
+require("dotenv").config();
+const serverless = require("serverless-http");
+const express = require("express");
+const request = require("request");
+
 const app = express();
+const API_URL = process.env.API_URL;
 
-app.use(bodyParser.json({ strict: false }));
+const getApiResults = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true }, (err, res, body) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(body);
+    });
+  });
+};
 
-app.get('/ialsearch', function (req, res) {
-  res.send('IAL search');
+app.get("/ialsearch", function (req, res) {
+  const params = new URLSearchParams(req.query).toString();
+  getApiResults(`${API_URL}/ialsearch?${params}`)
+    .then((data) => res.json(data))
+    .catch((err) =>
+      res.json({
+        error: {
+          msg: `Unable to connect to MAS IAL API due to ${err}`,
+        },
+      })
+    );
+});
+
+app.get("/ialsuggest", function (req, res) {
+  const params = new URLSearchParams(req.query).toString();
+  getApiResults(`${API_URL}/ialsuggest?${params}`)
+    .then((data) => res.json(data))
+    .catch((err) =>
+      res.json({
+        error: {
+          msg: `Unable to connect to MAS IAL API due to ${err}`,
+        },
+      })
+    );
 });
 
 module.exports.handler = serverless(app);
